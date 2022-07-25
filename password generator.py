2f5cce79fa3bcbf1aa26c4a567149b030a883c4a
Diksha Agrawal <agrawaldiksha631@gmail.com>
2022-07-25 17:05:30 +0530

    Initial commit

---
+++ password generator.py
@@ -0,0 +1,11 @@
+import random
+import string
+user = int(input('Enter the length of the password (atleast 8): '))
+print()
+guess = ''.join(random.choices(string.ascii_letters + string.digits, k=user))
+print(guess)
+if user >= 8:
+    for x in guess:
+        print('*', end='')
+else:
+    print('Password should be of atleast 8 characters!')
